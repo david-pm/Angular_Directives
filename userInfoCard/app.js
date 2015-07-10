6eada216d08c37562bf1c67282f4ffc0d36c9c52
davidpm <david@david-pm.com>
2015-07-10 12:23:03 -0700

    nesting directive with inherited scope inside a directive with isolated scope

--- userInfoCard/app.js
+++ userInfoCard/app.js
@@ -4,33 +4,63 @@
 
 	angular.module('app').controller('MainController', 
 		['$scope', function($scope){
-			$scope.user = {
+			$scope.user1 = {
 				name: "Han Solo",
 				address: {
-					street: 'Lightyear Lane',
+					street: '145 Lightyear Lane',
 					city: 'Secret Rebel Base',
 					planet: 'Yanvin 4'
 				},
 				friends: ["Luke", "Chewbacca", "Lando"]
 			};
-			console.log($scope); // shared $scope, inherited $scope, isolated $scope
+			$scope.user2 = {
+				name: 'Luke Skywalker',
+				address: {
+					street: '2001 Wormhole Avenue',
+					city: 'Mos Eisley',
+					planet: 'Tattoine'
+				},
+				friends: ["Han", "Chewbacca", "Leia"]
+			};
 			
 		} // controller fn
-	]);
+	]); // MainController
 
-	angular.module('app').directive('userInfoCard', [function(){
-		// Runs during compile
-		return {
-			templateUrl: "userinfocard.html",
-			scope: true, // false is default which is shared scope, set to true for inherited scope, set to {} for isolated scope
-			controller: function($scope) {
-				$scope.knightMe = function(user) {
-					user.rank = "Knight";
-				};
-				console.log($scope); // shared $scope, inherited $scope, isolated $scope
+	angular.module('app').directive('userInfoCard', 
+		[function(){
+			return {
+				templateUrl: "userinfocard.html",
+				scope: {
+					user: "="
+				}, // false is default which is shared scope, set to true for inherited scope, set to {} for isolated scope
+				controller: function($scope) {
+					$scope.collapsed = true;
+					$scope.knightMe = function(user) {
+						user.rank = "Knight";
+					};
+					$scope.collapse = function() {
+						$scope.collapsed = !$scope.collapsed;
+					};
+				} // directive controller
+			}; //return
+		}]); // directive
 
-			} // controller
-		};
-	}]);
+	angular.module('app').directive('theAddress', 
+		[function(){
+			return {
+				restrict: 'E',
+				scope: true, // inherited scope
+				templateUrl: 'address.html',
+				controller: function($scope) {
+					$scope.collapsed = true;
+					$scope.expandAddress = function() {
+						$scope.collapsed = false;
+					};
+					$scope.collapseAddress = function() {
+						$scope.collapsed = true;
+					};
+				} // directive controller
+			}; //return
+		}]); // directive
 
 })();
