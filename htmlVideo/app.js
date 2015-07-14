780c36c507956ccf85caeae73c620b7eb35a33ce
David PM <info@davidpm.io>
2015-07-13 23:24:10 -0700

    handle pause events with $apply

--- htmlVideo/app.js
+++ htmlVideo/app.js
@@ -3,12 +3,30 @@
 	angular.module('app', []);
 
 	angular.module('app').controller('MainController', ['$scope', function($scope){
-		
+		$scope.handlePause = function() {
+			console.log('video was paused');
+		};
 	}]);
+
+	angular.module('app').directive('eventPause', function() {
+		return {
+			restrict: 'A',
+			scope: {
+				eventPause: '&'
+			},
+			link: function(scope, el, attrs) {
+				el.on('pause', function(event) {
+					scope.$apply(function() {
+						scope.eventPause();
+					})
+				})
+			}
+		};
+	});
 
 	angular.module('app').directive('spacebarSupport', function(){
 		return {
-			link: function($scope, el, attrs) {
+			link: function(scope, el, attrs) {
 				$('body').on('keypress', function(evt) {
 					var vidEl = el[0];
 					if (evt.keyCode === 32) {
