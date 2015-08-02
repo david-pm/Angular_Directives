156e77f009e70bc918c1b4a5d55e7710aa39eb37
davidpm <davidmcdonald@americanapparel.net>
2015-08-02 21:26:06 +0000

    organized examples

---
+++ _examples/isolateNestedScopes/app.js
@@ -0,0 +1,41 @@
+
+	angular.module('app', []);
+
+	angular.module('app')
+	
+	.controller('MainController', function($scope){
+		$scope.items1 = {btn: 'click me', text: 'now click me >'};
+		$scope.items2 = {btn: 'press me', text: 'now press me >'};
+		$scope.collapsed = true;
+	})
+	
+	.directive('myDirective', function(){
+		return {
+			scope: {
+				items: '='
+			},
+			templateUrl: 'items.html',
+			controller: function($scope) {
+				$scope.collapsed = false;
+				$scope.collapse = function() {
+				   $scope.collapsed = !$scope.collapsed;
+				}
+			}
+		};
+	})
+	
+	.directive('nested', function(){
+		return {
+			// change this back to false (shared scope), and instead of creating
+			// its own local scope vars, it will be grabbing the vars of it parent
+			// which is myDirective
+			scope: true,
+			templateUrl: 'nested.html',
+			controller: function($scope) {
+				$scope.collapsed = false;
+				$scope.collapse = function() {
+				   $scope.collapsed = !$scope.collapsed;
+				}
+			}
+		};
+	});
