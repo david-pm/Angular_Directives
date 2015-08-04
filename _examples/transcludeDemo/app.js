d70ab184779d6de1d73a4f3e042cbfdadc54dc1a
davidpm <davidmcdonald@americanapparel.net>
2015-08-04 23:56:39 +0000

    update transcludeDemo

--- _examples/transcludeDemo/app.js
+++ _examples/transcludeDemo/app.js
@@ -1,31 +1,50 @@
 (function() {
 
-	angular.module('app', []);
+'use strict';
 
-	angular.module('app').controller('MainController', ['$scope', function($scope){
+angular
+	.module('app', [])
+
+	.controller('MainController', ['$scope', function($scope){
 		$scope.messages = {
 			text1: "this is message one",
 			text2: "this is message two"
-		}
-	}]);
+		};
+	}])
 
-	angular.module('app').directive('displayBox', [function(){
+	.directive('displayBox', [function(){
 		return {
 			transclude: true,
-			scope: true, // best practice to have inherited or isolated scope when using transclusion
+			scope: {}, // best practice to have isolated scope or at least
+					   // inherited scope when using transclusion
 			templateUrl: 'displayBox.html',
 			controller: function($scope){
 				$scope.hidden = false;
 				$scope.close = function() {
 					$scope.hidden = true;
 				}
-				// cant do this since we have inherited scope
+				$scope.openMe = function() {
+					if(!!$scope.hidden)
+						$scope.hidden = false;
+				};
+				// cant do this with inherited/isolated scope
 				$scope.messages = {
 					text1: "this is a holdup",
 					text2: "this is a hijack"
 				}
 			}
 		};
+	}])
+	
+	.directive("welcome", [function() {
+	  	return {
+	    	restrict: "E",
+	    	scope: {},
+	    	transclude: true,
+  			template: "<div>This is the welcome component</div> <ng-transclude></ng-transclude>" + 
+  					  "<hr /><div class='col-sm-6 col-sm-offset-3'> <img class='img-responsive'" +
+  					  " src='img/transclude_scope.png' /> </div>"
+	    };
 	}]);
 
 })();
