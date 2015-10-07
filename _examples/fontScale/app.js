2be41d129574b8f91a5d6ee038e4d4a0ffbf302a
davidpm <info@davidpm.io>
2015-10-06 23:01:39 -0700

    add comments to fontScale, rename userInfoCard example directories

--- _examples/fontScale/app.js
+++ _examples/fontScale/app.js
@@ -1,17 +1,24 @@
+(function() {
+	'use strict';
+	
+	angular.module('app', [])
 
-	angular.module('app', []);
+		.controller('MainController', function($scope){
+			$scope.size = 150;
+		})
 
-	angular.module('app').controller('MainController', function($scope){
-		$scope.size = 150;
-	});
+		.directive('fontScale', function(){
+			return {
+				link: function(scope, el, attrs) {
+					// since we bound the input field 
+					// to font-scale we can do this:
+					scope.$watch(attrs['fontScale'], function(newVal) {
+						//jqLite
+						el.css('font-size', newVal + '%');
+					})
+				}
+			};
+		});
 
-	angular.module('app').directive('fontScale', function(){
-		return {
-			link: function(scope, el, attrs) {
-				scope.$watch(attrs['fontScale'], function(newVal) {
-					//jqLite
-					el.css('font-size', newVal + '%');
-				})
-			}
-		};
-	});
+
+}());
