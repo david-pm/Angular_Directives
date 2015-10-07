d17c0acdb7a62d47bca783846ecde5154e0579c0
davidpm <info@davidpm.io>
2015-10-06 22:14:33 -0700

    clean up myClick

--- _examples/myClick/app.js
+++ _examples/myClick/app.js
@@ -1,21 +1,21 @@
 (function() {
 
-	angular.module('app', []);
+	angular.module('app', [])
 
-	angular.module('app').controller('MainController', ['$scope', function($scope){
+	.controller('MainController', ['$scope', function($scope){
 		$scope.data = {message: "I have not been clicked"};
 		$scope.clickHandler = function(p) {
 			p.message = "I have been clicked";
 		}
-	}]);
+	}])
 
-	angular.module('app').directive('myClick', ['$parse', function($parse){
+	.directive('myClick', ['$parse', function($parse){
 		return {
-			link: function(scope, iElm, iAttrs, controller) {
-				var fn = $parse(iAttrs['myClick']);
-				iElm.on('click', function() {
+			link: function(scope, elem, attrs) {
+				var getter = $parse(attrs['myClick']);
+				elem.on('click', function() {
 					scope.$apply(function() {
-						fn(scope);
+						getter(scope);
 					})
 				})	
 			}
