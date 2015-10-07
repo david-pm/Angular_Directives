7cdfd6fad3e1da92be3ca32a1e383c0636251b34
davidpm <info@davidpm.io>
2015-10-06 22:28:38 -0700

    add comment and explantion of approach, clean up

--- _starwars/userTile/app.js
+++ _starwars/userTile/app.js
@@ -1,34 +1,44 @@
 (function() {
 
-	angular.module('app', []);
+	angular.module('app', [])
 
-	angular.module('app').controller('MainController', ['$scope', function($scope){
+	.controller('MainController', ['$scope', function($scope){
 		$scope.user1 = {
 			name: "Click Me", 
 			selected: false
 		}
-	}]);
+	}])
 
-	angular.module('app').directive('userTile', [function(){
+	.directive('userTile', [function(){
 		return {
 			scope: {
 				user : '=',
 			}, 
 			templateUrl: 'userTile.html'
-			/*controller: function($scope){
-				$scope.select = function() {
-					$scope.user.selected = !$scope.user.selected;
-				}	
-			}*/
-		};
-	}]);
+			// controller: function($scope){
+			// 	$scope.select = function() {
+			// 		$scope.user.selected = !$scope.user.selected;
+			// 	}	
+			// }
 
-	angular.module('app').directive('userClickSelect', [function() {
+			/* 
+			** Doing something like this is no big deal but
+			** its easy to imagine how it could get out of 
+			** control with many lines of code bloating out
+			** your component directive's controller. To keep
+			** things lean we can extract that business 
+			** specific logic out into a decorator directive
+			*/
+		};
+	}])
+
+	.directive('userClickSelect', [function() {
 		return {
 			link: function(scope, el, attrs) {
 				el.on('click', function() {
-					scope.user.selected = !scope.user.selected;
-					scope.$apply();
+					scope.$apply(function() {
+						scope.user.selected = !scope.user.selected;
+					});
 				})
 			}
 		}
