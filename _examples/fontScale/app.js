44e4c69a24ef2b660d9377a7e0a961f8ffad9ed5
davidpm <info@davidpm.io>
2015-10-08 21:16:27 -0700

    clean up fontScale styles and comments

--- _examples/fontScale/app.js
+++ _examples/fontScale/app.js
@@ -1,24 +1,22 @@
 (function() {
 	'use strict';
-	
+
 	angular.module('app', [])
 
 		.controller('MainController', function($scope){
 			$scope.size = 150;
 		})
-
+		
 		.directive('fontScale', function(){
 			return {
 				link: function(scope, el, attrs) {
-					// since we bound the input field 
-					// to font-scale we can do this:
+					// since we bound the text to the input field's model
+					// ($scope.size) via the font-scale attr/directive we:
 					scope.$watch(attrs['fontScale'], function(newVal) {
-						//jqLite
 						el.css('font-size', newVal + '%');
 					})
 				}
 			};
 		});
-
 
 }());
