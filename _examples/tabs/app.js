c555c205f3aa32ad60169c79df7673b468e5386f
David McDonald <dmcdonald@sellbrite.com>
2015-10-11 14:51:55 -0700

    cleanup tabs example

--- _examples/tabs/app.js
+++ _examples/tabs/app.js
@@ -1,46 +1,47 @@
 
-	angular.module('app', []);
+	angular.module('app', [])
 
-	angular.module('app').controller('MainController', function($scope){
-		
-	});
+		.controller('MainController', function($scope){
+		})
 
-	angular.module('app').directive('swTabstrip', function(){
-		return {
-			scope: {}, 
-			transclude: true,
-			controller: function($scope) {
-				$scope.panes = [];
-				$scope.select = function(pane){
-					pane.selected = true;
-					$scope.panes.forEach(function(curPane) {
-						if(curPane !== pane) {
-							curPane.selected = false;
-						}
-					})
-				}
-
-				this.addPane = function(pane) {
-					$scope.panes.push(pane);
-					if ($scope.panes.length === 1) {
+		.directive('swTabstrip', function(){
+			return {
+				scope: {},
+				transclude: true,
+				controller: function($scope) {
+					$scope.panes = [];
+					$scope.select = function(pane){
 						pane.selected = true;
+						$scope.panes.forEach(function(curPane) {
+							if(curPane !== pane) {
+								curPane.selected = false;
+							}
+						})
 					}
-				}
-			}, // controller
-			templateUrl: 'swTabstrip.html'
-		};
-	});
+					// notice the method is on the ctrl not the ctrl.scope
+					this.includePane = function(pane) {
+						//console.log(pane);
+						$scope.panes.push(pane);
+						if ($scope.panes.length === 1) {
+							pane.selected = true;
+						}
+					}
+				}, // controller
+				templateUrl: 'swTabstrip.html'
+			};
+		})
 
-	angular.module('app').directive('swPane', function(){
-		return {
-			scope: {
-				title: '@'
-			}, 
-			transclude: true,
-			require: '^swTabstrip',
-			link: function(scope, el, attrs, tabstripCtrl) {
-				tabstripCtrl.addPane(scope);
-			},
-			templateUrl: 'swPane.html'
-		};
-	});
+		.directive('swPane', function(){
+			return {
+				scope: {
+					title: '@'
+				},
+				transclude: true,
+				require: '^swTabstrip',
+				link: function(scope, el, attrs, tabstripCtrl) {
+					//console.log(scope);
+					tabstripCtrl.includePane(scope); // scope.title
+				},
+				templateUrl: 'swPane.html'
+			};
+		});
