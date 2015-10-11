84340e6a512d3d2ca843b211b60ab9eccd846324
David McDonald <dmcdonald@sellbrite.com>
2015-10-11 15:56:26 -0700

    renmae basicTransclude to transclude_basics add transclusion_and_scope examples

---
+++ _examples/transclusion_and_scopes/two/app.js
@@ -0,0 +1,39 @@
+(function() {
+
+'use strict';
+
+angular
+	.module('app', [])
+
+	.controller('MainController', ['$scope', function($scope){
+    $scope.person = {
+        name: 'John Doe',
+        profession: 'Fake name'
+      };
+      $scope.header = 'Person';
+    }])
+
+    .directive('person', function() {
+      return {
+        restrict: 'EA',
+        scope: {
+          header: '='
+        },
+        transclude:true,
+        //template: '<div>This is not transcluded, btw.</div><div ng-transclude></div>',
+        link: function(scope, element, attrs, ctrl, transclude) {
+          scope.person = {
+            name: 'Directive Joe',
+            profession: 'Scope guy'
+          };
+
+          scope.header = 'Directive\'s header';
+					//transclude(scope.$parent, function(clone) {
+					transclude(scope, function(clone) {
+							element.append(clone);
+					})
+        }
+      };
+    });
+
+})();
