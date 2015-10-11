9e673c31d7fa514aaf07258ac55574b705cf141f
David McDonald <dmcdonald@sellbrite.com>
2015-10-11 13:48:45 -0700

    add some more comments and do some cleanup to masters_*

--- _starwars/masters_pre_post_link/app.js
+++ _starwars/masters_pre_post_link/app.js
@@ -1,44 +1,44 @@
 
-angular.module('app', []);
+angular.module('app', [])
 
-angular.module('app').controller('MainController', function($scope){
-	
-});
+	.controller('MainController', function($scope){
+	})
 
-angular.module('app').directive('emperor', [function(){
-	return {
-		scope: true,
-		link: {
-			pre: function(scope, el, attr) {
-				el.data('name', 'The Emperor'); // data() is jQuery fn
-				scope.master = 'The Emperor';
+	.directive('emperor', [function(){
+		return {
+			scope: true,
+			link: {
+				pre: function(scope, el, attr) {
+					el.data('name', 'The Emperor'); // data() is jQuery fn
+					scope.master = 'The Emperor';
+					console.log(scope.master + ' has no master');
+				}
 			}
-		}
-	};
-}]);
-
-angular.module('app').directive('vader', [function(){
-	return {
-		scope: true,
-		link: {
-			pre: function(scope, el, attr) {
-				el.data('name', 'Darth Vader'); // data() is jQuery fn
-				el.data('master', scope.master);
-				console.log('Vader\'s master is ' + scope.master);
+		};
+	}])
+	// remember these directives are nested in the view so the children inherit from the parents
+	.directive('vader', [function(){
+		return {
+			scope: true,
+			link: {
+				pre: function(scope, el, attr) {
+					el.data('name', 'Darth Vader'); // data(key, val) is jQuery fn
+					el.data('master', scope.master);
+					console.log('Vader\'s master is ' + scope.master);
+				}
 			}
-		}
-	};
-}]);
+		};
+	}])
 
-angular.module('app').directive('starKiller', [function(){
-	return {
-		scope: true,
-		link: {
-			post: function(scope, el, attr) {
-				el.data('name', 'Starkiller'); // data() is jQuery fn
-				el.data('master', scope.master);
-				console.log('Starkiller\'s master is ' + scope.master);
+	.directive('starKiller', [function(){
+		return {
+			scope: true,
+			link: {
+				post: function(scope, el, attr) {
+					el.data('name', 'Starkiller'); // data() is jQuery fn
+					el.data('master', scope.master);
+					console.log('Starkiller\'s master is ' + scope.master);
+				}
 			}
-		}
-	};
-}]);
+		};
+	}]);
--- _starwars/masters_require_ctrls/app.js
+++ _starwars/masters_require_ctrls/app.js
@@ -1,56 +1,56 @@
 
-angular.module('app', []);
+angular.module('app', [])
 
-angular.module('app').controller('MainController', function($scope){
-	
-});
+	.controller('MainController', function($scope){
 
-angular.module('app').directive('emperor', [function(){
-	var name = 'The Emperor';
-	return {
-		scope: true,
-		controller: function($scope) {
-			// must set props that we want to pass around
-			// on the ctrl itself, not its $scope
-			this.name = name;
-		},
-		link: {
-			pre: function(scope, el, attr) {
-				el.data('name', name); // data() is jQuery fn
+	})
+
+	.directive('emperor', [function(){
+		var name = 'The Emperor';
+		return {
+			scope: true,
+			controller: function($scope) {
+				// must set properties we want to pass around
+				// on the controller itself, not its $scope
+				this.name = name;
+			},
+			link: {
+				pre: function(scope, el, attr) {
+					el.data('name', name); // data(key, val) is jQuery fn
+				}
 			}
-		}
-	};
-}]);
+		};
+	}])
 
-angular.module('app').directive('vader', [function(){
-	var name = 'Darth Vader';
-	return {
-		scope: true,
-		require: '^emperor',
-		controller: function($scope) {
-			this.name = name;
-		},
-		link: function(scope, el, attr, emperorCtrl) {
-			el.data('name', name);
-			el.data('master', emperorCtrl.name);
-			console.log('Vader\'s master is ' + emperorCtrl.name);
-		}
-	};
-}]);
-
-angular.module('app').directive('starKiller', [function(){
-	return {
-		scope: true,
-		require: '?^vader', // can use special chars like with scope: {}
-		link: function(scope, el, attr, vaderCtrl) {
-			el.data('name', 'Starkiller');
-			if(!!vaderCtrl) {
-				el.data('master', vaderCtrl.name);
-				console.log('Starkiller\'s master is ' + vaderCtrl.name);
-			} else {
-				console.log('Starkiller has no master!');
+	.directive('vader', [function(){
+		var name = 'Darth Vader';
+		return {
+			scope: true,
+			require: '^emperor',
+			controller: function($scope) {
+				this.name = name;
+			},
+			link: function(scope, el, attr, emperorCtrl) {
+				el.data('name', name);
+				el.data('master', emperorCtrl.name);
+				console.log('Vader\'s master is ' + emperorCtrl.name);
 			}
-			
-		}
-	};
-}]);
+		};
+	}])
+
+	.directive('starKiller', [function(){
+		return {
+			scope: true,
+			require: '?^vader', // can use special chars like with scope: {}
+			link: function(scope, el, attr, vaderCtrl) {
+				el.data('name', 'Starkiller');
+				if(!!vaderCtrl) {
+					el.data('master', vaderCtrl.name);
+					console.log('Starkiller\'s master is ' + vaderCtrl.name);
+				} else {
+					console.log('Starkiller has no master!');
+				}
+
+			}
+		};
+	}]);
